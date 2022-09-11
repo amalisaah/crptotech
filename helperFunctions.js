3d1cb43d19db01797533a74a2d12dc1d3a1cb8a4
Stephen Paakow Aihoon <stephen.aihoon@amalitech.org>
2022-09-11 20:53:36 +0000

    refactored functions

--- helperFunctions.js
+++ helperFunctions.js
@@ -1,5 +1,5 @@
 //Test Case
-/*const data = [{
+const data = [{
     id: 1019,
     name: 'Stephen',
     age: 24
@@ -8,9 +8,14 @@
     id: 2019,
     name: 'Saah',
     age: 25
+},
+{
+    id: 1231,
+    name: "Liz",
+    age: 21
 }
 ]; 
-*/
+
 
 
 //This function returns data based on specified ID
@@ -18,7 +23,7 @@ const getCryptotById = (id) => {
     return data.find(element =>element.id = id)
 };
 
+//This function deletes data based
 const deleteElementById = (id) => {
-    return data.filter(element => element.id !== id)
+    return data.pop(data.findIndex(element => element.id !== id))
 };
-

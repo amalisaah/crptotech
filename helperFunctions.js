db89b6a44d366664ee70bdc41effa96c54a31d73
Stephen Paakow Aihoon <stephen.aihoon@amalitech.org>
2022-09-11 20:08:59 +0000

    Function for getting Crypto by ID

---
+++ helperFunctions.js
@@ -0,0 +1,19 @@
+//Test Case
+/*const data = [{
+    id: 1019,
+    name: 'Stephen',
+    age: 24
+},
+{
+    id: 2019,
+    name: 'Saah',
+    age: 25
+}
+]; 
+*/
+
+//This function returns data based on specified ID
+const getCryptotById = (id) => {
+    return data.find(element =>element.id = id)
+};
+

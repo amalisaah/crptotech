65a5f877f85fcc3a39c832e43f8fcedd94e0597e
amalisaah <saah.asiedu@amalitech.org>
2022-09-13 05:29:18 +0000

    coin-view page styling in progress

--- src/Components/View/View.js
+++ src/Components/View/View.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React,{useEffect,useState} from 'react';
+import axios from 'axios'
 import './View.css';
 import { FaStar } from 'react-icons/fa';
 import { FiArrowUpRight, FiArrowDownLeft  } from 'react-icons/fi';
@@ -7,41 +8,54 @@ import { FiArrowUpRight, FiArrowDownLeft  } from 'react-icons/fi';
 const View = (props)=>{
     
     const data=props.coin
+    
 
-    if (!data) return null 
-
-   console.log(data)
-
-    return (
+    return ( 
         <div className='view'>
-            <div className='details'>
-                <div className='card left'>
-                    <div className='name'>
+            <div className='details details-top'>
+                <div className='card card-left'>
+                    <div className='name-w'>
+                        {/* <img src={data[0].image}  alt="coin logo" /> */}
+                        <h1 className="coin-name-w">
+                        {data.name} <span className="span">{data.symbol.toUpperCase()}</span>
+                        </h1>
                         <button className="add-to-fav view-btn" aria-label="Add to favourite" >
                             <FaStar />
-                        </button>
-                        {/* <img src={data[0].image}  alt="coin logo" /> */}
-                        <h1 className="coin-name">
-                        {data.name} <span className="span">{data.symbol.toUpperCase()}</span>
-                        </h1>  
+                        </button>  
                     </div>
-                    <button className='view-btn'>{data.cmc_rank}</button>
+                    <div className='view-btns'>
+                        <button className='view-btn rank'>Rank {data.cmc_rank}</button>
+                        <button className='view-btn cap'>{data.quote.USD.market_cap_dominance}</button>
+                    </div>
                 </div>
 
-                <div className='card'>
+                <div className='card card-right'>
+                    <div className='price-label'>{data.name} Price ({data.symbol}) </div>
                     <div className='top'>
                        <div className='price'>
-                            <button className='view-btn'>Price</button>
                             <h2>${data.quote.USD.price.toLocaleString()}</h2>
+
+                            <button className='rate view-btn'>{data.quote.percent_change_7d < 0 ? (
+                                <span className='red-b'>
+                                    <FiArrowDownLeft className='icon' /> 
+                                    {data.quote.USD.percent_change_7d.toFixed(2)}
+                                </span>
+                                 ) : (
+                                <span className='green-b'>
+                                    <FiArrowUpRight className='icon' /> 
+                                    {data.quote.USD.percent_change_7d.toFixed(2)}
+                                </span>
+                                 )}
+                            </button>
                         </div>
-                        <button className='buy view-btn'>Buy</button> 
+                        <button className='buys view-btn'>Buy</button> 
                     </div>
 
-                    <div className='rates'>
+                    {/* <div className='rates'>
                     <button className='view-btn'>{data.quote.USD.percent_change_24h < 0 ? (
                         <span className='red'>
                             <FiArrowDownLeft className='icon' />
-                            {data[0].quote.USD.percent_change_24h.toFixed(2)}%
+                            {data.quote.USD.percent_change_24h.toFixed(2)}%
                         </span>
                      ) : (
                         <span className='green'>
@@ -62,7 +76,7 @@ const View = (props)=>{
                         </span>
                          )}
                     </button>
-                    </div>
+                    </div> */}
                     
                 </div>
             </div>
@@ -73,12 +87,20 @@ const View = (props)=>{
                     <div className='price'>${data.quote.USD.market_cap.toFixed(0).toLocaleString()}</div>
                 </div>
                 <div>
+                    <h3 className='title'>Circulating Supply</h3>
+                    <div className='price'>${(data.circulating_supply.toFixed(0)).toLocaleString()}</div>
+                </div>
+                <div>
                     <h3 className='title'>Total Supply</h3>
-                    <div className='price'>${(data.total_supply.toFixed(0)).toLocaleString()}</div>
+                    <div className='price'>${(data.max_supply.toFixed(0)).toLocaleString()}</div>
                 </div>
                 <div>
                     <h3 className='title'>Fully Diluted Market Cap</h3>
                     <div className='price'>${data.quote.USD.fully_diluted_market_cap.toLocaleString()}</div>
+                </div>
+                <div>
+                    <h3 className='title'>24h Volume</h3>
+                    <div className='price'>${data.quote.USD.volume_24h.toLocaleString()}</div>
                 </div>
                 
             </div>

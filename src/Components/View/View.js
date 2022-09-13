3853fc68e488dd0cfe3762ab88707a94156f14ba
amalisaah <saah.asiedu@amalitech.org>
2022-09-13 20:23:47 +0000

    unused variables deleted to be made deployable

--- src/Components/View/View.js
+++ src/Components/View/View.js
@@ -1,5 +1,4 @@
-import React,{useEffect,useState} from 'react';
-import axios from 'axios'
+import React from 'react';
 import './View.css';
 import { FaStar } from 'react-icons/fa';
 import { FiArrowUpRight, FiArrowDownLeft  } from 'react-icons/fi';

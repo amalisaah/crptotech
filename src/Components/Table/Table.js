ce049cf0be9751b939fcde70f088650a48baa09f
amalisaah <saah.asiedu@amalitech.org>
2022-09-18 19:28:58 +0000

    coin page made fully responsive

--- src/Components/Table/Table.js
+++ src/Components/Table/Table.js
@@ -35,6 +35,7 @@ const Table = ({data,head,onClick,addfav,SelCoin,star,favourite,watch})=>{
 
 
     return (
+        <>
         <table className="market-table">
             <thead className="table-head">
                 <tr className="table-row table-title">
@@ -53,7 +54,7 @@ const Table = ({data,head,onClick,addfav,SelCoin,star,favourite,watch})=>{
                         <i className="material-icons unwatched" onClick={addWish} id={Data.name} title='add to watchlist'>star</i>
                     </button>
                 </td> 
-                <th className="table-data rank" scope="row">{index+1}</th>
+                <th className="table-data rank num" scope="row">{index+1}</th>
                 <th className="table-data rank Adds" scope="row" id={Data.name} title='click to buy' onClick={handleClick }>+</th>
 
                  <td className="table-data name">
@@ -102,6 +103,90 @@ const Table = ({data,head,onClick,addfav,SelCoin,star,favourite,watch})=>{
         ))}
             </tbody>)}
         </table>
+        <div className="market-div">
+            {/* <thead className="table-head">
+                <div className="div-row div-title">
+                {head.map((topic,index) => (
+                    
+                <th className='div-heading' key={index} id={topic}>{topic}</th>))}
+                </div>
+            </thead> */}
+            
+            {data && 
+            (<div className="div-body">
+            {data.map((Data,index)=>(
+            <div className="div-row" key={index}>
+               <div className="div-data"  >
+                    <button className="add-to-fav" aria-label="Add to favourite" >
+                        <i className="material-icons unwatched" onClick={addWish} id={Data.name} title='add to watchlist'>star</i>
+                    </button>
+                </div> 
+                {/* <th className="div-data rank" scope="row">{index+1}</th>*/}
+                <div className="div-data  Adds"  id={Data.name} title='click to buy' onClick={handleClick }>+</div> 
+                <div className='one'>
+                    <div className="div-data name">
+                    {/*  <div className="wrapper">
+                        <img src={Data.image} width="20" height="20" alt="coin logo"  />*/}
+
+                        <h4 onClick={showCoin} title='view more details'>
+                            <Link to='/view' className="coin-name" id={Data.name}> {Data.name} <span className="span">{Data.symbol.toUpperCase()}</span></Link>
+                        </h4>
+                        {/* </div> */}
+                    </div> 
+                    <div className='centage'>
+                        <div className="div-data last-update 24h">{Data.quote.USD.percent_change_24h < 0 ? (
+                                <span className='red'>
+                                    <FiArrowDownLeft className='icon' /> 
+                                    {Data.quote.USD.percent_change_24h.toFixed(2)}%
+                                </span>
+                            ) : (
+                                <span className='green'>
+                                    <FiArrowUpRight className='icon' />
+                                    {Data.quote.USD.percent_change_24h.toFixed(2)}%
+                                </span>
+                                )}
+                        </div>
+
+                        <div className="div-data last-update sevd">{Data.quote.percent_change_7d < 0 ? (
+                                <span className='red'>
+                                    <FiArrowDownLeft className='icon' /> 
+                                    {Data.quote.USD.percent_change_7d.toFixed(2)}%
+                                </span>
+                            ) : (
+                                <span className='green'>
+                                    <FiArrowUpRight className='icon' /> 
+                                    {Data.quote.USD.percent_change_7d.toFixed(2)}%
+                                </span>
+                                )} 
+                        </div>
+                    </div>
+                </div> 
+                <div className="div-data last-price">${Data.quote.USD.price.toFixed(2)}</div>
+
+
+                <div className='mark'>
+                    <div className="div-data market-cap">${(Data.quote.USD.market_cap.toFixed(0)).toLocaleString()}</div>
+                    <div className="div-data market-vol">{Data.quote.USD.volume_change_24h < 0 ? (
+                                    <span className='red'>
+                                        <FiArrowDownLeft className='icon' /> 
+                                        {Data.quote.USD.volume_change_24h.toFixed(2)}%
+                                    </span>
+                                ) : (
+                                    <span className='green'>
+                                        <FiArrowUpRight className='icon' />
+                                        {Data.quote.USD.volume_change_24h.toFixed(2)}%
+                                    </span>
+                                    )}
+                            </div>
+                    
+                    {/* <div className='Purchase'> <button className='div-btn' onClick={handleClick} >Buy</button></div> */}
+                </div>
+            </div>
+           
+        ))}
+            </div>)}
+        </div>
+        </>
         
     )
 }

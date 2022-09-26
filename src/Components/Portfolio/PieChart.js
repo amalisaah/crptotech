f7e20cfb9c64c0cd43ae8a6fd6ea6de5035e931b
amalisaah <saah.asiedu@amalitech.org>
2022-09-26 20:00:21 +0000

    fixing the arc error

--- src/Components/Portfolio/PieChart.js
+++ src/Components/Portfolio/PieChart.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Pie } from "react-chartjs-2";
-// import { Chart as ChartJS } from "chart.js/auto";
+import {Chart, ArcElement} from 'chart.js'
+Chart.register(ArcElement);
 
 function PieChart({ chartData }) {
   return <Pie data={chartData} />;
